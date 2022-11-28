160468be3379cdcfd93aca2184eaf47c2126e231
PaulEdworthy <dev-edu.goawn@simplelogin.com>
2022-11-28 16:49:38 -0500

    add lots of stuff

---
+++ checkers/board.py
@@ -0,0 +1,29 @@
+import pygame
+from piece import Piece
+from checkers.constants import BLACK, WHITE, ROWS, COLS, RED, SQUARE_SIZE
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 12
+
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def create_board(self):
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) % 2):
+                    if row < 3:
+                        self.board[row].append(Piece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(Piece(row, col, RED))
+
+
---
+++ checkers/constants.py
@@ -0,0 +1,11 @@
+# import pygame
+
+WIDTH, HEIGHT = 900, 900
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
